5a494d632c41e0d8e6c6fd64c6c1d51a8f3975e5
juneate <roccopan@gmail.com>
2015-06-17 19:50:32 -0400

    added some weather info

--- js/current.js
+++ js/current.js
@@ -1,12 +1,35 @@
 // The API url and the city we want to check...
-var url = "http://api.openweathermap.org/data/2.5/weather?callback=?";
-var city = "Toronto,ON";
+var weather = "http://api.openweathermap.org/data/2.5/weather?callback=?";
 var units = "metric";
 
-// Go call the URL and pass 2 parameters (q=city, units=metric/imperial)
-$.getJSON(url, {'q':city, 'units':units}, function(data)
-{
-	// Dump all the data into the console
-	console.log(data);
+// When someones does a search
+$('button[name="go"]').on('click', function(e) {
+
+	// Get the city name from the search bar
+	var city = $('input[name="city"]').val();
+
+	// Pre request, setup a loading work or image
+	$('.city').text('loading...');
+
+	// Go call the URL and pass 2 parameters (q=city, units=metric/imperial)
+	$.getJSON(weather, {'q':city, 'units':units}, function(data)
+	{
+		// Dump the data
+		console.log(data);
+
+		// Change up the interface using the data we got
+		$('.city').text(data.name);
+		$('.temp').html(data.main.temp + "&deg;C");
+
+		// If the temp is above 0 go red, else go blue
+		if (data.main.temp > 0) {
+			$('body').css('background-color', '#EF665A');
+		}
+		else {
+			$('body').css('background-color', '#87D7EF');
+		}
+
+	});
 
 });
+
